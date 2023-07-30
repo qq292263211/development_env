a4c0ebc9dbd126daacf70640685be8072af95dd7
hongbo wang <292263211@qq.com>
2023-07-30 14:21:32 +0800

    换为中科院的源

---
+++ install_env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cd /etc/apt/
+sudo cp sources.list sources.list.bak
+# 指定要创建的文本文件的名称和路径
+file_name="sources.list"
+
+# 使用单引号括起来，以保持源内容的格式
+sourceList="  deb https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+deb https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src https://mirrors.ustc.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse"
+
+# 使用双引号将变量括起来，并在echo语句中输出变量的内容
+echo "$sourceList" > "$file_name"
+sudo apt-get update
+sudo apt-get upgrade
+echo "换源成功!"
+
